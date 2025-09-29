9f587f9ab769c37d1c4334156de1e06d01f32dce
TetianaHumeniuk <Tetiana.Humeniuk.ShI.2023@lpnu.ua>
2025-09-29 10:41:28 +0300

    set main

--- information_protection/information_protection/urls.py
+++ information_protection/information_protection/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    # path('lab1/', include('lcg.urls')),
+    # path('lab2/', include('lab2.urls')),
+    # path('lab3/', include('lab3.urls')),
+    # path('lab4/', include('lab4.urls')),
+    # path('lab5/', include('lab5.urls')),
 ]
+
